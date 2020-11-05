1936b11230087d619e971d7ea6cf606cea29782a
t262g <t262gkyoutarou@gmail.com>
2020-11-05 17:15:24 +0900

    JSの修正

--- app/javascript/increase.js
+++ app/javascript/increase.js
@@ -1,16 +1,16 @@
 function increase () {
   const btn = document.getElementById('increase');
   btn.addEventListener('click', () => {
-    const place = document.getElementById('btns');
+    const place = document.getElementById('buttons');
     const HTML = `        
       <div class="quiz-set">
         <div class="quiz-input">
-          <label class="quiz-label" for="quiz_management_question${num}">問題</label>
-          <textarea class="quiz-area" placeholder="例) ゾウ" name="quiz_management[question${num}]" id="quiz_management_question${num}"></textarea>
+          <label class="quiz-label" for="quiz_management_question">問題</label>
+          <textarea class="quiz-area" placeholder="例) ゾウ" name="quiz_management[${num}][question]" id="quiz_management_${num}_question"></textarea>
         </div>
         <div class="quiz-input">
-          <label class="quiz-label" for="quiz_management_answer${num}">答え</label>
-          <input class="quiz-field" placeholder="例) elephant" type="text" name="quiz_management[answer${num}]" id="quiz_management_answer${num}" />
+          <label class="quiz-label" for="quiz_management_answer">答え</label>
+          <input class="quiz-field" placeholder="例) elephant" type="text" name="quiz_management[${num}][answer]" id="quiz_management_${num}_answer" />
         </div>
       </div>`;
     place.insertAdjacentHTML("beforebegin", HTML);
@@ -18,5 +18,5 @@ function increase () {
   });
 }
 
-let num = 6
+let num = 5
 window.addEventListener('load', increase);
