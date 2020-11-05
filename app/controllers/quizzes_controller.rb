ac40b8d478d44059047a17f6517c6fba19a25852
t262g <t262gkyoutarou@gmail.com>
2020-11-05 17:15:06 +0900

    コントローラーの記述

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,41 @@
 class QuizzesController < ApplicationController
   def index
   end
+
+  def new
+    @quiz_management = QuizManagement.new
+  end
+
+  def create
+    @quiz_sets = []
+    create_quiz_sets
+    @quiz_management = QuizManagement.new(quiz_params)
+    if @quiz_management.valid?
+      @quiz_management.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_quiz_sets
+    i = 0
+    while params[:quiz_management]["#{i}"]
+      quiz_set = params[:quiz_management]["#{i}"].permit(:question, :answer).to_h
+      if !(quiz_set[:question].blank?) && !(quiz_set[:answer].blank?)
+        @quiz_sets << quiz_set
+      end
+      i += 1
+    end
+
+    if @quiz_sets.length < 3
+      @quiz_sets = nil
+    end
+  end
+
+  def quiz_params
+    params.require(:quiz_management).permit(:title, :explanation).merge(quiz_sets: @quiz_sets, user_id: current_user.id)
+  end
 end
