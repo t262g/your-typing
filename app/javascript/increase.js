c6b1dc44e181d30bdf0979f8b3ce8aab3a45fa92
t262g <t262gkyoutarou@gmail.com>
2020-11-13 12:39:10 +0900

    JavaScriptファイルを適用させるページの条件の記述

--- app/javascript/increase.js
+++ app/javascript/increase.js
@@ -1,22 +1,26 @@
-function increase () {
-  const place = document.getElementById("all_quiz");
-  let index = place.childElementCount;
-  const btn = document.getElementById('increase');
-  btn.addEventListener('click', () => {
-    const HTML = `        
-      <div class="quiz-set">
-        <div class="quiz-input">
-          <label class="quiz-label" for="quiz_management_question">問題</label>
-          <textarea class="quiz-area" placeholder="例) ゾウ" name="quiz_management[${index}][question]" id="quiz_management_${index}_question"></textarea>
-        </div>
-        <div class="quiz-input">
-          <label class="quiz-label" for="quiz_management_answer">答え</label>
-          <input class="quiz-field" placeholder="例) elephant" type="text" name="quiz_management[${index}][answer]" id="quiz_management_${index}_answer" />
-        </div>
-      </div>`;
-    place.insertAdjacentHTML("beforeend", HTML);
-    index += 1;
-  });
-}
+if (document.URL.match( /quizzes.*new/ ) || document.URL.match( /quizzes.*edit/ )) {
 
-window.addEventListener('load', increase);
+  function increase () {
+    const place = document.getElementById("all_quiz");
+    let index = place.childElementCount;
+    const btn = document.getElementById('increase');
+    btn.addEventListener('click', () => {
+      const HTML = `        
+        <div class="quiz-set">
+          <div class="quiz-input">
+            <label class="quiz-label" for="quiz_management_question">問題</label>
+            <textarea class="quiz-area" placeholder="例) ゾウ" name="quiz_management[${index}][question]" id="quiz_management_${index}_question"></textarea>
+          </div>
+          <div class="quiz-input">
+            <label class="quiz-label" for="quiz_management_answer">答え</label>
+            <input class="quiz-field" placeholder="例) elephant" type="text" name="quiz_management[${index}][answer]" id="quiz_management_${index}_answer" />
+          </div>
+        </div>`;
+      place.insertAdjacentHTML("beforeend", HTML);
+      index += 1;
+    });
+  }
+
+  window.addEventListener('load', increase);
+
+}
--- app/javascript/play_quiz.js
+++ app/javascript/play_quiz.js
@@ -1,44 +1,48 @@
-function initialize () {
-  question.innerHTML = '';
-  answer.value = '';
-  quizSet = lists[i]
-  const HTML = `${quizSet.question}`;
-  question.insertAdjacentHTML('afterbegin', HTML);
-}
+if (document.URL.match( /quizzes.+\d$/ )) {
 
-function judge () {
-  console.log(`quiz${i}実行中`);
-  if (quizSet.answer === answer.value) {  
-    console.log(`問題${i}正解です`);
-    i++;
-
-    if (length == i) {
-      clearInterval(time);
-      result();
-      return;
-    }
-
-    initialize();
+  function initialize () {
+    question.innerHTML = '';
+    answer.value = '';
+    quizSet = lists[i]
+    const HTML = `${quizSet.question}`;
+    question.insertAdjacentHTML('afterbegin', HTML);
   }
+
+  function judge () {
+    console.log(`quiz${i}実行中`);
+    if (quizSet.answer === answer.value) {  
+      console.log(`問題${i}正解です`);
+      i++;
+
+      if (length == i) {
+        clearInterval(time);
+        result();
+        return;
+      }
+
+      initialize();
+    }
+  }
+
+  function result () {
+    question.innerHTML = 'クイズ終了です';
+    answer.value = '';
+  }
+
+  function play () {
+    console.log('playQuizが呼ばれました')
+    const quizValue = json_quiz.value
+    lists = JSON.parse(quizValue);
+    length = lists.length;
+    question = document.getElementById('question');
+    answer = document.getElementById('answer');
+    i = 0;
+    
+    initialize();
+
+    time = setInterval(judge, 300);
+  }
+
+  window.addEventListener('load', play);
+
 }
-
-function result () {
-  question.innerHTML = 'クイズ終了です';
-  answer.value = '';
-}
-
-function play () {
-  console.log('playQuizが呼ばれました')
-  const quizValue = json_quiz.value
-  lists = JSON.parse(quizValue);
-  length = lists.length;
-  question = document.getElementById('question');
-  answer = document.getElementById('answer');
-  i = 0;
-  
-  initialize();
-
-  time = setInterval(judge, 300);
-}
-
-window.addEventListener('load', play);
