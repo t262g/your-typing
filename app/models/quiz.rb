edbf7d3489ffc39700e2a28adccf565e36f22ffe
t262g <t262gkyoutarou@gmail.com>
2020-11-15 22:21:22 +0900

    アソシエーションの記述

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
   belongs_to :user
   has_many :quiz_lists, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  has_many :quizzes
+  has_many :quizzes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
