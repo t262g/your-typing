ab414be30af2f9ac21d7ec0a9e944d9e3b66d66e
t262g <t262gkyoutarou@gmail.com>
2020-11-15 21:13:29 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'quizzes#index'
-  resources :quizzes
+  resources :quizzes do
+    resources :comments, only: [:new, :create]
+  end
   resources :users, only: :show
 end
