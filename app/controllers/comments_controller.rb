163be96beede159296e37954012e348e5152e6f6
t262g <t262gkyoutarou@gmail.com>
2020-11-15 22:55:02 +0900

    各コントローラーの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @quiz = Quiz.find(params[:quiz_id])
+    @comment = @quiz.comments.new(comment_params)
+    if @comment.save
+      redirect_to quiz_path(@quiz)
+    else
+      render "quizzes/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id) 
+  end
 end
