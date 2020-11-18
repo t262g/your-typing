f7b8776e6f3cd7a204361c9f0f81f2b4968ec991
t262g <t262gkyoutarou@gmail.com>
2020-11-18 23:48:57 +0900

    JSの修正

--- app/javascript/play_quiz.js
+++ app/javascript/play_quiz.js
@@ -1,44 +1,49 @@
-function initialize () {
-  question.innerHTML = '';
-  answer.value = '';
-  quizSet = lists[i]
-  const HTML = `${quizSet.question}`;
-  question.insertAdjacentHTML('afterbegin', HTML);
-}
-
-function judge () {
-  if (quizSet.answer === answer.value) {  
-    i++;
-
-    if (length == i) {
-      clearInterval(time);
-      result();
-      return;
-    }
-
-    initialize();
-  }
-}
-
-function result () {
-  question.innerHTML = 'クイズ終了です';
-  answer.value = '';
-}
-
 function play () {
   const json = document.getElementById('json_quiz');
   if ( json != null) {
     const quizValue = json.value
-    lists = JSON.parse(quizValue);
-    length = lists.length;
-    question = document.getElementById('question');
-    answer = document.getElementById('answer');
-    i = 0;
-    
-    initialize();
+    const lists = JSON.parse(quizValue);
+    let index = 0;
+  
+    reset(lists, index);
 
-    time = setInterval(judge, 300);
+    const time = setInterval( () => {
+      const questionArea = document.getElementById('question_area');
+      const quizSet = lists[index];
+      const answerArea = document.getElementById("answer_area");
+      const answer = document.getElementById('answer');
+      const condition1 = quizSet.answer == answer.value
+      const condition2 = lists.length == index + 1
+
+      // //回答が正しかったときの問題の更新処理
+      if (condition1 && !condition2) {
+        index++;
+        reset(lists, index);
+      }
+    
+      // //最後の問題を回答したときの処理
+      if (condition1 && condition2) {
+        clearInterval(time);
+        questionArea.innerHTML = 'クイズ終了です';
+        answerArea.innerHTML = '';
+      }
+    }, 200, [lists, index, time]);
   }
+}
+
+function reset (lists, index) {
+  //問題のリセット
+  const questionArea = document.getElementById('question_area');
+  questionArea.innerHTML = '';
+  const quizSet = lists[index];
+  questionArea.innerHTML = `${quizSet.question}`;
+
+  //回答欄のリセット
+  const answerArea = document.getElementById("answer_area");
+  answerArea.innerHTML = '';
+  answerArea.innerHTML = '<input id="answer"></input>';
+  const answer = document.getElementById('answer');
+  answer.focus();
 }
 
 window.addEventListener('load', play);
