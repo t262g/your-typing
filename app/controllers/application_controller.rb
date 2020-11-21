bcabd64bdc26d47416ee263a0971d2371cc9239d
t262g <t262gkyoutarou@gmail.com>
2020-11-21 15:32:18 +0900

    ストロングパラメーターの追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :image])
   end
 end
