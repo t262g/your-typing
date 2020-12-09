e403ca2a5476f336ef0267f6879f4073b1f315cd
t262g <t262gkyoutarou@gmail.com>
2020-12-09 17:41:26 +0900

    Quizzesコントローラーの修正

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,18 +8,29 @@ class QuizzesController < ApplicationController
   end
 
   def new
-    @quiz_management = QuizManagement.new
+    @quiz = Quiz.new
   end
+  # def new
+  #   @quiz_management = QuizManagement.new
+  # end
 
   def create
-    build_quiz_management
-    if @quiz_management.valid?
-      @quiz_management.save
-      redirect_to root_path
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
+      redirect_to new_quiz_quiz_list_path(@quiz)
     else
       render :new
     end
   end
+  # def create
+  #   build_quiz_management
+  #   if @quiz_management.valid?
+  #     @quiz_management.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
   def show
     @comment = Comment.new
@@ -29,20 +40,20 @@ class QuizzesController < ApplicationController
     @json_info = [current_user.nickname, @quiz.id].to_json if user_signed_in?
   end
 
-  def edit
-    @quiz_sets = @quiz_lists
-    @quiz_management = QuizManagement.new
-  end
+  # def edit
+  #   @quiz_sets = @quiz_lists
+  #   @quiz_management = QuizManagement.new
+  # end
 
-  def update
-    build_quiz_management
-    if @quiz_management.valid?
-      @quiz_management.update(@quiz)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   build_quiz_management
+  #   if @quiz_management.valid?
+  #     @quiz_management.update(@quiz)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     @quiz.destroy
@@ -51,24 +62,28 @@ class QuizzesController < ApplicationController
 
   private
 
-  def build_quiz_management
-    @quiz_sets = []
-    create_quiz_sets
-    @quiz_management = QuizManagement.new(quiz_params)
-  end
+  # def build_quiz_management
+  #   @quiz_sets = []
+  #   create_quiz_sets
+  #   @quiz_management = QuizManagement.new(quiz_params)
+  # end
 
-  def create_quiz_sets
-    i = 0
-    while params[:quiz_management][i.to_s]
-      quiz_set = params[:quiz_management][i.to_s].permit(:question, :answer).to_h
-      @quiz_sets << quiz_set if !quiz_set[:question].blank? && !quiz_set[:answer].blank?
-      i += 1
-    end
-  end
+  # def create_quiz_sets
+  #   i = 0
+  #   while params[:quiz_management][i.to_s]
+  #     quiz_set = params[:quiz_management][i.to_s].permit(:question, :answer).to_h
+  #     @quiz_sets << quiz_set if !quiz_set[:question].blank? && !quiz_set[:answer].blank?
+  #     i += 1
+  #   end
+  # end
 
   def quiz_params
-    params.require(:quiz_management).permit(:title, :explanation).merge(quiz_sets: @quiz_sets, user_id: current_user.id)
+    params.require(:quiz).permit(:title, :explanation).merge(user_id: current_user.id)
   end
+
+  # def quiz_params
+  #   params.require(:quiz_management).permit(:title, :explanation).merge(quiz_sets: @quiz_sets, user_id: current_user.id)
+  # end
 
   def find_quiz
     @quiz = Quiz.find(params[:id])
