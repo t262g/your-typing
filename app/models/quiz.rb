ff9d3489e8a0e3466871766f6ba0c30db37e71f4
t262g <t262gkyoutarou@gmail.com>
2020-12-09 17:25:55 +0900

    Quizモデルへのバリデーションの記述

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,8 @@ class Quiz < ApplicationRecord
   belongs_to :user
   has_many :quiz_lists, dependent: :destroy
   has_many :comments, dependent: :destroy
+  with_options presence: true do
+    validates :title
+    validates :explanation
+  end
 end
